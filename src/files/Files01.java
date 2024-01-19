4004c17e502920a88ecf2dfbe113e691b9a76058
Arosha Rukshan <rukshanarosha@gmail.com>
2024-01-19 15:02:39 +0530

    initial commit

---
+++ src/files/Files01.java
@@ -0,0 +1,15 @@
+package files;
+
+import java.io.*;
+
+public class Files01 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader r = new BufferedReader(new FileReader("data/wolf-fox.txt"));
+
+    r.lines().forEach(l -> System.out.println(l));
+
+    r.close();
+  }
+
+}
